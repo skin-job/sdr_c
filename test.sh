dbd082d7e726d9065166fe3b1746692f7ee52494
SkinJob <skin-job@github.com>
2017-08-07 14:40:33 -0300

    Removed python scripts, changed compiler options for test.sh 	deleted:    do_dvbs2map.py 	deleted:    do_dvbs2map_unpacked.py 	modified:   test.sh 	modified:   test_unpacked.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc do_dvbs2map.c -O3 -march=native -Wall -o do_dvbs2map
+gcc do_dvbs2map.c -O3 -march=native -fomit-frame-pointer -Wall -o do_dvbs2map
 cat /dev/zero | pv -r | ./do_dvbs2map > /dev/null
--- test_unpacked.sh
+++ test_unpacked.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc do_dvbs2map_unpacked.c -O3 -march=native -Wall -o do_dvbs2map_unpacked
+gcc do_dvbs2map_unpacked.c -O3 -march=native -fomit-frame-pointer -Wall -o do_dvbs2map_unpacked
 cat /dev/zero | pv -r | ./do_dvbs2map_unpacked > /dev/null
